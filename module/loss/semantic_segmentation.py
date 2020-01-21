45f552ea31e7c36bd88ad0ae2156a6cd83aaf90d
Joyako <dylan1221cs@163.com>
2020-01-21 21:42:05 +0800

    fix bug

--- module/loss/semantic_segmentation.py
+++ module/loss/semantic_segmentation.py
@@ -12,22 +12,28 @@ class SegmentationLosses(nn.Module):
         self.ce = nn.CrossEntropyLoss(weight=weight, reduction='mean',
                                       ignore_index=ignore_index)
 
-    def forward(self, logit, target):
+    def forward(self, preds, target):
         """Choices: ['CE' or 'FL']"""
+        H1, W1 = preds.size()[2:]
+        H2, W2 = target.size()[1:]
+        assert H1 == H2 and W1 == W2
+
+        C = preds.size(1)
+        preds = preds.transpose(1, 2).transpose(2, 3).contiguous().view(-1, C)
+        target = target.view(-1)
+
         if self.mode == 'CE':
-            return self.CrossEntropyLoss(logit, target)
+            return self.CrossEntropyLoss(preds, target)
         elif self.mode == 'FL':
-            return self.FocalLoss(logit, target)
+            return self.FocalLoss(preds, target)
         else:
             raise NotImplementedError
 
-    def CrossEntropyLoss(self, logit, target):
-        mask = target >= 0
-        return self.ce(logit, target.long())
+    def CrossEntropyLoss(self, preds, target):
+        return self.ce(preds, target)
 
-    def FocalLoss(self, logit, target):
-
-        logpt = -self.ce(logit, target.long())
+    def FocalLoss(self, preds, target):
+        logpt = -self.ce(preds, target.long())
         pt = torch.exp(logpt)
         if self.alpha is not None:
             logpt *= self.alpha
