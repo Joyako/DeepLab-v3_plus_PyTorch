34003e29959e0d6526743fcb7289adde2bef07f6
Joyako <dylan1221cs@163.com>
2020-02-01 20:52:30 +0800

    add validate function

--- train/fcn_lane_segmentation.py
+++ train/fcn_lane_segmentation.py
@@ -1,14 +1,16 @@
 import time
 import torch
 from torch.optim import Adam, SGD
-from module.backbone_network.FCN import FCN8s, FCN16s, FCN32s
 from torch.optim.lr_scheduler import MultiStepLR
 import torch.nn.functional as F
+
+from module.backbone_network.FCN import FCN8s, FCN16s, FCN32s
+from module.utils.metric import Metric
 
 
 def train(net, data_loader, optimizer, criterion, device, epoch, print_freq=40):
     net.train()
-
+    num_data = len(data_loader)
     for i_batch, sample in enumerate(data_loader):
         start_time = time.time()
         img, target = sample['image'], sample['label']
@@ -17,9 +19,8 @@ def train(net, data_loader, optimizer, criterion, device, epoch, print_freq=40):
 
         optimizer.zero_grad()
         output = net(img)
-        output = F.softmax(output, dim=1)
 
-        loss = criterion(output, target)
+        loss = criterion(output, target.long())
 
         loss.backward()
         optimizer.step()
@@ -27,14 +28,36 @@ def train(net, data_loader, optimizer, criterion, device, epoch, print_freq=40):
         end_time = time.time() - start_time
         lr = optimizer.param_groups[0]['lr']
         if i_batch % print_freq == 0:
-            print("Epoch: {} Iter: {}/{} Loss: {:.4f} LR: {:.8f} BatchTime: {:.4f}".format(epoch,
-                                                                                           i_batch, len(data_loader),
-                                                                                           loss.item(),
-                                                                                           lr,
-                                                                                           end_time))
+            print("Train/Epoch: {} Iter: {}/{} Loss: {:.4f} LR: {:.8f} BatchTime: {:.4f}".format(epoch,
+                                                                                                 i_batch, num_data,
+                                                                                                 loss.item(),
+                                                                                                 lr,
+                                                                                                 end_time))
 
 
-def build_train(cfg, data_loader,
+def validation(net, data_loader, criterion, device, epoch, metric, print_freq):
+    net.eval()
+    num_data = len(data_loader)
+    for i_batch, sample in enumerate(data_loader):
+        with torch.no_grad():
+            img, target = sample['image'], sample['label']
+            img = img.to(device)
+            target = target.to(device)
+
+            outputs = net(img)
+            outputs = F.softmax(outputs)
+            loss = criterion(outputs, target.long())
+            preds = outputs.data.max(1)[1].cpu().numpy()
+            mIoU = metric.mIoU(preds=preds, target=target.cpu().numpy())
+
+        if i_batch % print_freq == 0:
+            print("Validate/Epoch: {} Iter: {}/{} Loss: {:.4f} mIoU: {:.4f}".format(epoch,
+                                                                                i_batch, num_data,
+                                                                                loss.item(),
+                                                                                mIoU))
+
+
+def build_train(cfg, data_loader, val_loader,
                 criterion, device):
     if cfg['mode'] == 'fcn-8s':
         net = FCN8s(cfg['num_classes'])
@@ -62,9 +85,12 @@ def build_train(cfg, data_loader,
         raise NotImplementedError
 
     lr_scheduler = MultiStepLR(optimizer, milestones=cfg['milestones'], gamma=0.1)
+    metric = Metric(cfg['num_classes'])
 
     for epoch in range(1, cfg['epoch'] + 1):
         train(net, data_loader, optimizer, criterion, device, epoch, cfg['print_freq'])
+        validation(net, val_loader, criterion, device, epoch, metric, cfg['print_freq'])
+
         lr_scheduler.step()
         if epoch > 50 and epoch % 10 == 0:
             torch.save(net.state_dict(), cfg['save_path'] + '/Lane_%s.pth' % epoch)
