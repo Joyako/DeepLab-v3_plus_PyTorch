80fcea5ccc202f18c3688bb3b4b97c8ab4904cfc
Joyako <dylan1221cs@163.com>
2020-02-03 13:58:14 +0800

    add evluate metric

--- train/fcn_lane_segmentation.py
+++ train/fcn_lane_segmentation.py
@@ -2,13 +2,12 @@ import time
 import torch
 from torch.optim import Adam, SGD
 from torch.optim.lr_scheduler import MultiStepLR
-import torch.nn.functional as F
 
 from module.backbone_network.FCN import FCN8s, FCN16s, FCN32s
 from module.utils.metric import Metric
 
 
-def train(net, data_loader, optimizer, criterion, device, epoch, print_freq=40):
+def train(net, data_loader, optimizer, criterion, device, epoch, metric, print_freq=40):
     net.train()
     num_data = len(data_loader)
     for i_batch, sample in enumerate(data_loader):
@@ -18,43 +17,47 @@ def train(net, data_loader, optimizer, criterion, device, epoch, print_freq=40):
         target = target.to(device)
 
         optimizer.zero_grad()
-        output = net(img)
+        outputs = net(img)
 
-        loss = criterion(output, target.long())
+        loss = criterion(outputs, target.long())
 
         loss.backward()
         optimizer.step()
 
+        preds = outputs.data.max(1)[1].cpu().numpy()
+        mIoU = metric.mIoU(preds=preds, target=target.cpu().numpy())
         end_time = time.time() - start_time
         lr = optimizer.param_groups[0]['lr']
         if i_batch % print_freq == 0:
-            print("Train/Epoch: {} Iter: {}/{} Loss: {:.4f} LR: {:.8f} BatchTime: {:.4f}".format(epoch,
-                                                                                                 i_batch, num_data,
-                                                                                                 loss.item(),
-                                                                                                 lr,
-                                                                                                 end_time))
+            print("Train/Epoch: {} Iter: {}/{} Loss: {:.4f} mIoU: {:.4f} "
+                  "LR: {:.8f} BatchTime: {:.4f}".format(epoch,
+                                                        i_batch, num_data,
+                                                        loss.item(), mIoU,
+                                                        lr,
+                                                        end_time))
 
 
 def validation(net, data_loader, criterion, device, epoch, metric, print_freq):
     net.eval()
     num_data = len(data_loader)
-    for i_batch, sample in enumerate(data_loader):
-        with torch.no_grad():
+    confusion_matrix = 0.
+    with torch.no_grad():
+        for i_batch, sample in enumerate(data_loader):
             img, target = sample['image'], sample['label']
             img = img.to(device)
             target = target.to(device)
 
             outputs = net(img)
-            outputs = F.softmax(outputs)
+            # outputs = F.softmax(outputs)
             loss = criterion(outputs, target.long())
             preds = outputs.data.max(1)[1].cpu().numpy()
-            mIoU = metric.mIoU(preds=preds, target=target.cpu().numpy())
+            confusion_matrix = metric.add(preds=preds, target=target.cpu().numpy(), m=confusion_matrix)
 
-        if i_batch % print_freq == 0:
-            print("Validate/Epoch: {} Iter: {}/{} Loss: {:.4f} mIoU: {:.4f}".format(epoch,
-                                                                                i_batch, num_data,
-                                                                                loss.item(),
-                                                                                mIoU))
+            if i_batch % print_freq == 0:
+                print("Validate/Epoch: {} Iter: {}/{} Loss: {:.4f}".format(epoch,
+                                                                           i_batch, num_data,
+                                                                           loss.item()))
+        print("mIoU : {}".format(metric.mIoU(m=confusion_matrix)))
 
 
 def build_train(cfg, data_loader, val_loader,
@@ -88,7 +91,7 @@ def build_train(cfg, data_loader, val_loader,
     metric = Metric(cfg['num_classes'])
 
     for epoch in range(1, cfg['epoch'] + 1):
-        train(net, data_loader, optimizer, criterion, device, epoch, cfg['print_freq'])
+        train(net, data_loader, optimizer, criterion, device, epoch, metric, cfg['print_freq'])
         validation(net, val_loader, criterion, device, epoch, metric, cfg['print_freq'])
 
         lr_scheduler.step()
