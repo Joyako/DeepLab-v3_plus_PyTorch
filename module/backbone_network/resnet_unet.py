1cf1f9f963e60f0da52b6a9ad45705f9608465bd
Joyako <dylan1221cs@163.com>
2020-02-19 10:01:23 +0800

    oh my gold, I forget

--- module/backbone_network/resnet_unet.py
+++ module/backbone_network/resnet_unet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+import torch.nn.functional as F
 
 
 class Decoder(nn.Module):
@@ -121,6 +122,27 @@ class ResNet_UNet(nn.Module):
             nn.Sigmoid(),
         )
 
+        # Spatial transformer localization-network
+        self.localization = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(10 * 52 * 52, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
         self._init_weight()
 
     def _init_weight(self):
@@ -133,7 +155,22 @@ class ResNet_UNet(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
+    def stn(self, x):
+        # Spatial transformer network forward function
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 52 * 52)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
+
     def forward(self, x):
+        # transform the input
+        # x = self.stn(x)
+
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
